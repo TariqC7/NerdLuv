0b6f013917678534bd4c64ece66dbba3be920346
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2019-04-01 22:54:57 -0400

    Creating sign up form cont'd
    Accepting personality type, OS preference and seeking age

--- signup.php
+++ signup.php
@@ -42,3 +42,53 @@ Header logo and form to create new account
                 <strong>Age:</strong>
                 <input type="text" name="age" size="6" maxlength="2"/>
             </li>
+            
+            <li>
+                <!--
+                Personality type: A 6-character-wide text box allowing
+                the user to type a Keirsey personality type, such as ISTJ or
+                ENFP. The box should let the user type up to 4
+                characters. The label has a link to
+                http://www.humanmetrics.com/cgi-win/JTypes2.asp.
+                -->
+                <strong>Personality type:</strong>
+                <input type="text" name="type" size="6" maxlength="4"/>
+                <a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">(Don't know your type?)</a>
+            </li>
+
+            <li>
+                <!--
+                Favorite OS: A drop-down select box allowing the user to
+                select a favorite operating system. The choices are Windows,
+                Mac OS X, and Linux. Initially "Windows" is selected.
+                -->
+                <strong>Favorite OS:</strong>
+                <select name="OS">
+                    <option selected="selected">Windows</option>
+                    <option>Mac OS X</option>
+                    <option>Linux</option>
+                </select>
+            </li>
+
+            <li>
+                <!--
+                Seeking age: Two 6-character-wide text boxes for the user to
+                specify the range of acceptable ages of partners.The box should allow
+                the user to type up to 2 characters in each box. Initially both are
+                empty and have placeholder text of "min" and "max" respectively.
+                When the user starts typing, this placeholder text disappears.
+                -->
+                <strong>Seeking Age:</strong>
+                <input type="text" name="min" size="6" maxlength="2" placeholder="min"/> to
+                <input type="text" name="max" size="6" maxlength="2" placeholder="max"/>
+            </li>
+        </ul>
+
+        <!--
+        Sign Up: When the user presses "Sign Up," the form should submit its data as a POST to signup-submit.php.
+        -->
+        <input type="submit" value="Sign Up"/>
+    </fieldset>
+</form>
+
+<?php include("bottom.html"); ?>
